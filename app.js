ade6bf9348bc8e9093a8fb763cf7d71d71efb61e
Niraj Tiwari <tniraj7@gmail.com>
2019-02-25 20:27:27 +0900

    Refactored the code

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const request = require('request');
 const yargs = require('yargs');
 
+
+const API_KEY = "1bf7b4bd00bd3c84b2ca2d73fa0ddcd3";
+const BASE_URL = "https://api.openweathermap.org/data/2.5/weather?q="
+
 const argv = yargs
 .options({
     City : {
@@ -14,14 +18,13 @@ const argv = yargs
 .alias('help', 'h')
 .argv;
 
-
-
-const URL = "https://api.openweathermap.org/data/2.5/weather?q=tokyo&appid=1bf7b4bd00bd3c84b2ca2d73fa0ddcd3";
+const CITY = encodeURIComponent(argv.City);
+const URL = `${BASE_URL}${CITY}&appid=${API_KEY}`;
 
 request({
     url: URL,
     json: true
 },(error, response, body) => {
-        console.log(JSON.stringify(body.weather[0].description, undefined,2));
+        console.log(`Weather in ${argv.City} is `, JSON.stringify(body.weather[0].description, undefined,2));
     }
 );
