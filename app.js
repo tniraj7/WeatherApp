787fc54c8922656f27a4dbc195cdaf6928c087c0
Niraj Tiwari <tniraj7@gmail.com>
2019-02-26 23:12:01 +0900

    refactored code and exported the weather function as module

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const request = require('request');
 const yargs = require('yargs');
-
+const cityWeather = require('./playground/weather/weather');
 
 const API_KEY = "1bf7b4bd00bd3c84b2ca2d73fa0ddcd3";
 const BASE_URL = "https://api.openweathermap.org/data/2.5/weather?q="
@@ -21,22 +21,10 @@ const argv = yargs
 const CITY = encodeURIComponent(argv.City);
 const URL = `${BASE_URL}${CITY}&appid=${API_KEY}`;
 
-request({
-    url: URL,
-    json: true
-}, (error, response, body) => {
-    if (error) {
-        console.log("Unable to connect to OpenWeathermaps API");
-    } else if (response) {
-        if (response.statusCode === 401) {
-            console.log("\nInvalid API key");
-        } else if (response.statusCode === 404) {
-            console.log("\nCity not found");
-        } else if (response.statusCode === 501) {
-            console.log("\nMaintenance mode");
-        } else if (response.statusCode === 200) {
-            console.log("\nRequest successfull!");
-            console.log(`\nWeather in ${argv.City} is `, JSON.stringify(body.weather[0].description, undefined, 2));
-        }
+cityWeather.getCityWeather(URL, (errorMessage, results) => {
+    if(errorMessage) {
+        console.log(errorMessage);
+    } else {
+        console.log(JSON.stringify(results, undefined, 2));
     }
 });
